8ee889b8919232e1b457564c6dad64d2484e6847
qianchuming <73211139+qianchuming@users.noreply.github.com>
2020-11-18 11:54:28 +0800

    Add files via upload

---
+++ dianming2.py
@@ -0,0 +1,15 @@
+f=open('C:/Users/user/Desktop/Walden.txt','r',encoding='utf-8')
+line = f.read()
+#将文本拆分为word
+words = line.split()
+#print(words)
+new_words = []
+for word in words:
+    new_words.append(word.replace(',','').replace('.','').replace(':','').replace(';','').lower())
+words_set = set(new_words)
+words_dict= {}
+for word in words_set:
+    #print(word,words.count(word))
+    words_dict[word] = new_words.count(word)
+sorted(words_dict.items(),key=lambda x:x[1], reverse=True)
+#print(words_dict.items())
