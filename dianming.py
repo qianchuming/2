e0b17772a2020eab66e30eb7939c3f4c262bb960
qianchuming <73211139+qianchuming@users.noreply.github.com>
2020-11-25 08:39:50 +0800

    Update dianming.py

--- dianming.py
+++ dianming.py
@@ -1,21 +1,15 @@
 students = {}
 f= open('C:/Users/user/Desktop/students.csv','r')
 lines=f.readlines()
-#print(lines)
 for line in lines:
     tmp1=line.split(',')
-    #print(tmp1)
     xuehao = tmp1[0]
     xingming = tmp1[1]
     students[xuehao] = xingming
     #students[line.split(',')[0]] = line.split(',')[1]
-#print(students)
 f.close()
 import random
 num = eval(input(''))
-#print(students.keys())
-#for key in students.keys():
-#    print(key)
 results=random.sample(students.keys(),num)
 for xuehao in results:
     print(students[xuehao])
